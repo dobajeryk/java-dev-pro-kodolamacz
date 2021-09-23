32c011899f84e5da44b95430b8b8d8a07738f4a1
Eryk Dobaj <dobajeryk@gmail.com>
2021-09-23 22:13:03 +0200

    Empty project

---
+++ src/main/java/com/example/kodolamacztask/KodolamaczTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodolamacztask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodolamaczTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodolamaczTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kodolamacztask/KodolamaczTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodolamacztask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodolamaczTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
